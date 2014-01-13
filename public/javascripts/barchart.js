a1a53042f51223f24f8a0a0ce8d6566f40748ee8
Pierre <pierre.besson7@gmail.com>
2014-01-13 23:31:04 +0100

    simple bar chart

---
+++ public/javascripts/barchart.js
@@ -0,0 +1,14 @@
+//Define the data for the bar chart.
+var data = [4, 8, 15, 16, 23, 42];
+
+//Define a range in d3.
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
